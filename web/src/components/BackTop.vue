1a82fcbeb0bd7a50c9d31c429bae66700e2164e7
mstmdev <mstmdev@gmail.com>
2023-07-11 01:00:26 +0800

    Add the back top button

---
+++ web/src/components/BackTop.vue
@@ -0,0 +1,3 @@
+<template>
+  <a-back-top />
+</template>
--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import TheConfiguration from '../components/TheConfiguration.vue'
+import BackTop from '../components/BackTop.vue'
 </script>
 <template>
   <TheConfiguration />
+  <BackTop />
 </template>
