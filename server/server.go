fa53c4536aea390755aacf2bfc314c2560c78776
mstmdev <mstmdev@gmail.com>
2023-07-18 00:04:34 +0800

    Support open browser on Linux and macOS (#15)

--- server/server.go
+++ server/server.go
@@ -17,9 +17,21 @@ func Run() {
 	log.Log("start web server => %s", url)
 	go func() {
 		time.Sleep(time.Second)
-		if err == nil && runtime.GOOS == "windows" {
-			exec.Command("cmd", "/c", "start", url).Start()
+		if err == nil {
+			log.ErrorIf(openBrowser(url), "open url error => %s", url)
 		}
 	}()
 	err = log.ErrorIf(http.ListenAndServe(addr, http.FileServer(http.FS(dist.WebDist))), "start web server error")
+}
+
+func openBrowser(url string) (err error) {
+	switch runtime.GOOS {
+	case "windows":
+		err = exec.Command("cmd", "/c", "start", url).Start()
+	case "darwin":
+		err = exec.Command("open", url).Start()
+	case "linux":
+		err = exec.Command("xdg-open", url).Start()
+	}
+	return err
 }
