fff3ecd7bc091a3750e3971a81a55c68ac27aa32
mstmdev <mstmdev@gmail.com>
2023-07-25 00:19:17 +0800

    Add the NumberInput component

---
+++ web/src/components/NumberInput.vue
@@ -0,0 +1,15 @@
+<template>
+  <a-input-number
+    v-model:value="value"
+    :min="0"
+    :max="99999"
+    @change="$emit('update:value', value)"
+  />
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const props = defineProps<{ value: number }>()
+const value = ref(props.value)
+</script>
