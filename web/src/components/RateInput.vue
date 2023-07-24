33e07bf416604ac1d4b29a854ffdec494b6cb6ef
mstmdev <mstmdev@gmail.com>
2023-07-25 00:55:42 +0800

    Add the RateInput component (#26)

---
+++ web/src/components/RateInput.vue
@@ -0,0 +1,30 @@
+<template>
+  <a-row>
+    <a-col :span="12">
+      <a-slider
+        v-model:value="value"
+        :min="0"
+        :max="1"
+        :step="0.01"
+        @change="$emit('update:value', value)"
+      />
+    </a-col>
+    <a-col :span="4">
+      <a-input-number
+        v-model:value="value"
+        :min="0"
+        :max="1"
+        :step="0.01"
+        style="margin-left: 16px"
+        @change="$emit('update:value', value)"
+      />
+    </a-col>
+  </a-row>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const props = defineProps<{ value: number }>()
+const value = ref(props.value)
+</script>
