a730174ec3a13f101d1a5c84658fb2cb2ff63d72
mstmdev <mstmdev@gmail.com>
2023-07-25 14:08:28 +0800

    Add the unplugin-vue-components plugin to reduce release size (#34)

--- web/src/main.ts
+++ web/src/main.ts
@@ -2,7 +2,6 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import Antd from 'ant-design-vue'
 
 import App from './App.vue'
 import router from './router'
@@ -11,6 +10,5 @@ import { i18n } from './locales/locales'
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
-app.use(Antd)
 app.use(i18n)
 app.mount('#app')
