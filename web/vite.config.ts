0b5af95751dbd50e20a5e2f7ae071b75a27b6657
mstmdev <mstmdev@gmail.com>
2023-07-25 13:55:47 +0800

    Add the unplugin-vue-components plugin to reduce release size

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,11 +2,22 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite';
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    Components({
+      resolvers: [
+        AntDesignVueResolver({
+          importStyle: false, // css in js
+        }),
+      ],
+      dts: path.resolve('src/types/components.d.ts')
+    }),
   ],
   resolve: {
     alias: {
